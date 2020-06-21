899612315e038b4b6974e0cc757ef1bf469d1af4
Md-Masum <66910507+1834902579@users.noreply.github.com>
2020-06-20 23:30:50 -0700

    Add files via upload

---
+++ Link_List/1.c
@@ -0,0 +1,24 @@
+// Define a LinkList with three variable and print this link list ( without using function,DMA,typedef)//
+#include<stdio.h>
+struct node {
+
+    int data;
+    struct node* next;
+
+};
+int main ()
+{
+
+    struct node a,b,c;
+
+    a.data = 10;
+    a.next = &b;
+    b.data = 10;
+    b.next = &c;
+    c.data = 10;
+    c.next = NULL;
+
+    printf("%p\t%d\t%p",&a,a.data,a.next);
+    printf("\n%p\t%d\t%p",&b,b.data,b.next);
+    printf("\n%p\t%d\t%p",&c,c.data,c.next);
+}
